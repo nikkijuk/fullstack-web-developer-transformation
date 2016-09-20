7591b8f00d9f8022689bab4d5e064416c0c7c492
jukka nikki <jukka.nikki@kolumbus.fi>
2016-09-20 16:51:39 +0200

    week 2 - forms & form validation

--- fs-angular1-dev/conFusion/app/scripts/app.js
+++ fs-angular1-dev/conFusion/app/scripts/app.js
@@ -78,4 +78,42 @@ angular.module('confusionApp', [])
 	    $scope.showDetails = !$scope.showDetails;
   };
   
+}])
+
+// contact
+
+.controller('ContactController', ['$scope', function($scope) {
+
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+
+    $scope.channels = channels;
+    
+    $scope.invalidChannelSelection = false;
+}])
+
+// feedback
+
+.controller('FeedbackController', ['$scope', function($scope) {
+
+	 $scope.sendFeedback = function() {
+     
+		 console.log($scope.feedback);
+         
+		 if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+             $scope.invalidChannelSelection = true;
+             console.log('incorrect');
+         }
+         else {
+             $scope.invalidChannelSelection = false;
+             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+             
+             $scope.feedback.mychannel="";
+
+             $scope.feedbackForm.$setPristine();
+             
+             console.log($scope.feedback);
+         }
+     };
 }]);
