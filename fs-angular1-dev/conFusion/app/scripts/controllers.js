48d8f7c9587fce0e0986de223a9eedc253b94f6e
jukka nikki <jukka.nikki@kolumbus.fi>
2016-09-27 15:05:42 +0200

    week 3 - ui-router

--- fs-angular1-dev/conFusion/app/scripts/controllers.js
+++ fs-angular1-dev/conFusion/app/scripts/controllers.js
@@ -64,11 +64,9 @@ angular.module('confusionApp')
                 }
             };
         }])
-
-        .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
-
-            var dish = menuFactory.getDish(parseInt($routeParams.id,10));
-            
+        
+      .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             $scope.dish = dish;
         }])
         
