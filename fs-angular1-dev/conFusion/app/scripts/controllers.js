1a2b491dcde2d9c247dfa82f4069edff5d44b51a
Jukka Nikki <jukka.nikki@kolumbus.fi>
2016-10-10 22:28:50 +0200

    * tests, etc. for rest of angular course - committed as bunch while git on other computer won't anymore push commits

--- fs-angular1-dev/conFusion/app/scripts/controllers.js
+++ fs-angular1-dev/conFusion/app/scripts/controllers.js
@@ -1,181 +1,208 @@
 'use strict';
 
 angular.module('confusionApp')
+    .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {            
+        
+    	$scope.tab = 1;
+        $scope.filtText = '';
+        $scope.showDetails = false;
+                                
+        $scope.showMenu = false;
+        $scope.message = "Loading ...";
+        
+        menuFactory.getDishes().query(
+            function(response) {
+                $scope.dishes = response;
+                $scope.showMenu = true;
+            },
+            function(response) {
+                $scope.message = "Error: "+response.status + " " + response.statusText;
+            });
+                    
+        $scope.select = function(setTab) {
+            $scope.tab = setTab;
+            
+            if (setTab === 2) {
+                $scope.filtText = "appetizer";
+            }
+            else if (setTab === 3) {
+                $scope.filtText = "mains";
+            }
+            else if (setTab === 4) {
+                $scope.filtText = "dessert";
+            }
+            else {
+                $scope.filtText = "";
+            }
+        };
 
-        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {            
-            
-        	$scope.tab = 1;
-            $scope.filtText = '';
-            $scope.showDetails = false;
-                                    
-            $scope.showMenu = false;
-            $scope.message = "Loading ...";
-            $scope.dishes= {};
-            
-            menuFactory.getDishes()
-            .then(
-                function(response) {
-                    $scope.dishes = response.data;
-                    $scope.showMenu = true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
-            
-            $scope.select = function(setTab) {
-                $scope.tab = setTab;
-                
-                if (setTab === 2) {
-                    $scope.filtText = "appetizer";
-                }
-                else if (setTab === 3) {
-                    $scope.filtText = "mains";
-                }
-                else if (setTab === 4) {
-                    $scope.filtText = "dessert";
-                }
-                else {
-                    $scope.filtText = "";
-                }
-            };
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
+        };
 
-            $scope.isSelected = function (checkTab) {
-                return ($scope.tab === checkTab);
-            };
-    
-            $scope.toggleDetails = function() {
-                $scope.showDetails = !$scope.showDetails;
-            };
-        }])
+        $scope.toggleDetails = function() {
+            $scope.showDetails = !$scope.showDetails;
+        };
+    }])
 
-        .controller('ContactController', ['$scope', function($scope) {
+    .controller('ContactController', ['$scope', function($scope) {
 
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-            
-            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-            
-            $scope.channels = channels;
-            $scope.invalidChannelSelection = false;
-                        
-        }])
+        $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+        
+        var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+        
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+                    
+    }])
 
-        .controller('FeedbackController', ['$scope', function($scope) {
+    .controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope, feedbackFactory) {
+        
+        $scope.sendFeedback = function() {
             
-            $scope.sendFeedback = function() {
-                
+            console.log($scope.feedback);
+            
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            } else {
+            	
+                // save new feedback object
+                feedbackFactory.getFeedback().save($scope.feedback);
+
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                $scope.feedback.mychannel="";
+                $scope.feedbackForm.$setPristine();
                 console.log($scope.feedback);
-                
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
-                    $scope.invalidChannelSelection = true;
-                    console.log('incorrect');
-                } else {
-                    $scope.invalidChannelSelection = false;
-                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-                    $scope.feedback.mychannel="";
-                    $scope.feedbackForm.$setPristine();
-                    console.log($scope.feedback);
-                }
-            };
-        }])
-        
-      .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-          $scope.dish = {};
-          $scope.showDish = false;
-          $scope.message="Loading ...";
-          //console.log ("id "+parseInt($stateParams.id,10));
-          
-          menuFactory.getDish(parseInt($stateParams.id,10))
-          .then(
-              function(response){
-                  $scope.dish = response.data;
-                  $scope.showDish=true;
-              },
-              function(response) {
-                  $scope.message = "Error: "+response.status + " " + response.statusText;
-              }
-          );
-                      
-        }])
-        
-      .controller('DishCommentController', ['$scope', function($scope) {
+            }
+        };
+    }])
+    
+  .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+      $scope.dish = {};
+      $scope.showDish = false;
+      $scope.message="Loading ...";
+      $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+        .$promise.then(
+          function(response){
+              $scope.dish = response;
+              $scope.showDish = true;
+          },
+          function(response) {
+              $scope.message = "Error: "+response.status + " " + response.statusText;
+          }
+ 		);        
+   }])
+    
+    .controller('DishCommentController', ['$scope', 'menuFactory', function($scope,menuFactory) {
 
-        	// list possible ratings for radio buttons
+    	// list possible ratings for radio buttons
            $scope.ratings = [0,1,2,3,4,5];
  
         	var createEmptyComment = function  () {
         		// this variable is local and new is created each time when fuction is called 
-            	var emptyComment = {
-                        rating:5,
-                        comment:"",
-                        author:"",
-                        date:""
-                    }
-            	
-            	// return fresh variable
-            	return emptyComment;
-        		
-        	}
+        	var emptyComment = {
+                    rating:5,
+                    comment:"",
+                    author:"",
+                    date:""
+                }
         	
-            //Step 1: Create a JavaScript object to hold the comment from the form
+        	// return fresh variable
+        	return emptyComment;
+    		
+    	}
+    	
+        //Step 1: Create a JavaScript object to hold the comment from the form
+    	$scope.newComment = createEmptyComment(); 
+    	
+        $scope.submitComment = function () {
+            
+            //Step 2: This is how you record the date
+        	$scope.newComment.date = new Date().toISOString();
+            
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push($scope.newComment);
+
+            // update existing dish 
+            menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
+            
+            //Step 4: reset your form to pristine
+            // doesn't two way binding handle this automatically when we use $scope.newComment ?
+            // so: I see this is needed only when form is not bound to object which is shared between controller and view
+            $scope.commentForm.$setPristine();
+            
+            // show success in log
+            console.log('Saved feedback from '+$scope.newComment.author);
+            
+            //Step 5: reset your JavaScript object that holds your comment
         	$scope.newComment = createEmptyComment(); 
         	
-            $scope.submitComment = function () {
-                
-                //Step 2: This is how you record the date
-            	$scope.newComment.date = new Date().toISOString();
-                
-                // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push($scope.newComment);
-                
-                //Step 4: reset your form to pristine
-                // doesn't two way binding handle this automatically when we use $scope.newComment ?
-                // so: I see this is needed only when form is not bound to object which is shared between controller and view
-                $scope.commentForm.$setPristine();
+        }
+    }])
 
-                // show success in log
-                console.log('Saved feedback from '+$scope.newComment.author);
-                
-                //Step 5: reset your JavaScript object that holds your comment
-            	$scope.newComment = createEmptyComment(); 
-            	
-            }
-        }])
+   // implement the IndexController and About Controller here
+    
+  .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
 
-       // implement the IndexController and About Controller here
-        
-      .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
- 
-    	  // lazy implementation: bind all directly to scope -- methods could be better here, but ...
-    	  // it's not clear from assignement if "with data obtained through the controller" means data can be delivered thru $scope
-    	  
-       	  $scope.leader = corporateFactory.getLeader (3);
-       	  
-    	  $scope.promotion = menuFactory.getPromotion(0);
-    	  
-          $scope.dish = {};
-          $scope.showDish = false;
-          $scope.message="Loading ...";
-
-          menuFactory.getDish(0)
-          .then(
+      $scope.showLeader = false;
+	  
+	  // get CEO (id=3)          
+   	  $scope.leader = corporateFactory.getLeaders().get({id:3})
+   	  	.$promise.then(
               function(response){
-                  $scope.dish = response.data;
-                  $scope.showDish = true;
+                  $scope.leader = response;
+                  $scope.showLeader = true;
               },
               function(response) {
                   $scope.message = "Error: "+response.status + " " + response.statusText;
               }
-          );
-      }])
+	 		); 
 
-      .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
+      $scope.showPromotion = false;
 
-    	  // lazy implementation: bind all directly to scope -- methods could be better here, but ...
-    	  // it's not clear from assignement if "with data obtained through the controller" means data can be delivered thru $scope
-    	  
-    	  $scope.leaders = corporateFactory.getLeaders ();
-    	      	      	  
-      }])
-        
-;
+      // get first promotion (id=0)
+	  $scope.promotion = menuFactory.getPromotions().get({id:0})
+ 	  	.$promise.then(
+                function(response){
+                    $scope.promotion = response;
+                    $scope.showPromotion = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+  	 		); 
+
+	  $scope.showDish = false;
+      $scope.message="Loading ...";
+      
+      // get dishes
+      $scope.dish = menuFactory.getDishes().get({id:0})
+      .$promise.then(
+          function(response){
+              $scope.dish = response;
+              $scope.showDish = true;
+          },
+          function(response) {
+              $scope.message = "Error: "+response.status + " " + response.statusText;
+          }
+      );
+      
+  }])
+
+  .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
+
+      $scope.showLeaders = false;
+
+      // get leaders
+	  $scope.leaders = corporateFactory.getLeaders().query(
+              function(response) {
+                  $scope.leaders = response;
+                  $scope.showLeaders = true;
+              },
+              function(response) {
+                  $scope.message = "Error: "+response.status + " " + response.statusText;
+              }
+      	);    	  
+  }]);
